2ede4190e9f5a49ab6607527856335c79022e16b
Jess Woodward <jw1519@student.aru.ac.uk>
2025-03-11 14:53:26 +0000

    made CardSlot abstract

--- Assets/script/Cards/CardSlots/CardSlots.cs
+++ Assets/script/Cards/CardSlots/CardSlots.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CardSlots : MonoBehaviour, IDropHandler
+public abstract class CardSlots : MonoBehaviour, IDropHandler
 {
     public List<GameObject> cards;
     public virtual void OnDrop(PointerEventData eventData)
