82385946e532937da4bc721c3f4d7284d9cfc046
Jess Woodward <jw1519@student.aru.ac.uk>
2025-03-14 11:20:04 +0000

    close panel is done through button for difficulty panel

--- Assets/script/panels/DifficultyPanel.cs
+++ Assets/script/panels/DifficultyPanel.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using UnityEngine;
 
 public class DifficultyPanel : BasePanel
 {
@@ -9,24 +7,20 @@ public class DifficultyPanel : BasePanel
     public void Awake()
     {
         setDifficulty = gameObject.GetComponent<SetDifficulty>();
-        OpenPanel();
     }
     public void Easy()
     {
         difficulty = Enemy.EnemyDifficulty.Easy;
         setDifficulty.SetUp(difficulty);
-        ClosePanel();
     }
     public void Medium()
     {
         difficulty = Enemy.EnemyDifficulty.Medium;
         setDifficulty.SetUp(difficulty);
-        ClosePanel();
     }
     public void Hard()
     {
         difficulty = Enemy.EnemyDifficulty.Hard;
         setDifficulty.SetUp(difficulty);
-        ClosePanel();
     }
 }
