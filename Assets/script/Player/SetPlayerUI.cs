ee98ff235d0ef95bf9f96f9719b520a5b137f019
Jess <jw1519@student.aru.ac.uk>
2025-03-18 12:16:06 +0000

    added a health bar

--- Assets/script/Player/SetPlayerUI.cs
+++ Assets/script/Player/SetPlayerUI.cs
@@ -1,14 +1,17 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetPlayerUI : MonoBehaviour
 {
     public TextMeshProUGUI playerHealthtext;
     public TextMeshProUGUI defenceText;
+    public Slider healthBar;
 
     public void UpdatePlayerHealth(Player player)
     {
         playerHealthtext.SetText(player.health.ToString() + "/" + player.maxHealth);
+        healthBar.value = player.health;
         if (player.health <= 0)
         {
             Debug.Log("GameOver");
