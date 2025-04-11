b1c1a834e01bf06cf773c90808d7dc52d5872e3d
Jess Woodward <JW1519@student.aru.ac.uk>
2025-04-11 13:26:51 +0100

    edited UI

--- Assets/script/Cards/CardSlots/PlayedCards.cs
+++ Assets/script/Cards/CardSlots/PlayedCards.cs
@@ -7,7 +7,7 @@ public class PlayedCards : CardSlots
         for (int i = 0; i < cards.Count; i++)
         {
             cards[i].transform.localPosition = new Vector2(-200 + i * 70, 0);
-            cards[i].transform.localScale = new Vector2(0.7f, 0.7f);
+            cards[i].transform.localScale = new Vector2(0.75f, 0.8f);
         }
     }
 }
