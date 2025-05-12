799817d79051d3b35e92a54e3aa42388a50b8141
Jess <jw1519@student.aru.ac.uk>
2025-05-12 14:46:41 +0100

    got rid of comment

--- Assets/script/Cards/Card/DragAndDrop.cs
+++ Assets/script/Cards/Card/DragAndDrop.cs
@@ -18,7 +18,6 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
     public void OnBeginDrag(PointerEventData eventData)
     {
         isDragging = true;
-        //parentAfterDrag = transform.parent;
         parentAfterDrag.gameObject.GetComponent<CardSlots>().cards.Remove(gameObject);
         transform.SetParent(transform.root);
         canvasGroup.blocksRaycasts = false;
